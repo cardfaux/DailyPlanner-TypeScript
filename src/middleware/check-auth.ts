f6fceb489e9344ecf1206267d5f17ea958573f63
James Hagood <cardfaux@tutanota.com>
2020-03-20 13:06:42 -0500

    changed check-auth

--- src/middleware/check-auth.ts
+++ src/middleware/check-auth.ts
@@ -2,16 +2,16 @@ import jwt from 'jsonwebtoken';
 
 import HttpError from '../models/http-error';
 
-export default (req, res, next) => {
+export default (req: any, res: any, next: any) => {
 	if (req.method === 'OPTIONS') {
 		return next();
 	}
 	try {
 		const token = req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
 		if (!token) {
-			throw new Error('No Token, Authentication failed!', 401);
+			throw new Error('No Token, Authentication failed!');
 		}
-		const decodedToken = jwt.verify(token, `${process.env.JWT_KEY}`);
+		const decodedToken: any = jwt.verify(token, `${process.env.JWT_KEY}`);
 		req.userData = { userId: decodedToken.userId };
 		next();
 	} catch (err) {
